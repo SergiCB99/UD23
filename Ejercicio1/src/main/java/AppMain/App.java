76bb7a25883d1cd8f5c3699684583ceaf7f0a65b
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-11 13:03:51 +0200

    Create App.java

---
+++ Ejercicio1/src/main/java/AppMain/App.java
@@ -0,0 +1,14 @@
+package AppMain;
+
+import Models.Sentencias;
+import Views.ViewPrincipal;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ViewPrincipal app = new ViewPrincipal();
+		
+	}
+	
+}
